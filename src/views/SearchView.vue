4b11738b6a959770547275465541193094b0299d
vibha <alurvibha@gmail.com>
2023-11-09 22:54:27 +0530

    added current rating

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,23 +2,24 @@
   <div >
     <h1>Brewery Search</h1>
     
-    <input v-model="city" class="form-control mb-3" placeholder="Search by City" @change="getBreweriesByCity(city)">
-    <input v-model="name" class="form-control mb-3" placeholder="Search by Name" @change="getBreweriesByName(name)">
-    <input v-model="type" class="form-control mb-3" placeholder="Search by Type" @change="getBreweriesByType(type)">
+    <input v-model="city" class="form-control mb-3" placeholder="Search by City" @change="getBreweriesByCity">
+    <input v-model="name" class="form-control mb-3" placeholder="Search by Name" @change="getBreweriesByName">
+    <input v-model="type" class="form-control mb-3" placeholder="Search by Type" @change="getBreweriesByType ">
     
     <div class="row">
       <div v-for="i in 3" :key="i" class="col-md-4 mb-3">
         <div class="card border-dark mb-3" style="max-width: 18rem;" v-for="(brewery, index) in breweries.slice((i-1)*3, i*3)" :key="index" @click="viewBrewery(brewery)">
         <div class="card-body" >
             <h5 class="card-title">{{ brewery.name }}</h5>
-            <p class="card-text">Address: {{ brewery.street }}</p>
+            <p class="card-text">Address: {{ brewery.street }}, {{ brewery.city }}</p>
+            <p class="card-text">Type: {{ brewery.brewery_type }}</p>
             <p class="card-text">Phone: {{ brewery.phone }}</p>
-            <p class="card-text">Website: <a :href="brewery.website_url" target="_blank">{{ brewery.website_url }}</a></p>
             <p class="card-text">
-        Current Rating: {{ calculateAverageRating(brewery.id) }}
+            Current Rating: {{ brewery.currentRating }}
             </p>
-            <p class="card-text">{{ brewery.city }}, {{ brewery.state }}</p>
-      
+            <p class="card-text">{{ brewery.state}}, {{ brewery.country }}</p>
+            <p class="card-text">Website: <a :href="brewery.website_url" target="_blank">{{ brewery.website_url }}</a></p>
+            
             <!-- Other details you want to display -->
         </div>
         </div>
@@ -34,54 +35,61 @@ import { loadReviews } from '@/firebase/firestore.js';
 export default {
   data() {
     return {
-        name: '',
-        city: '',
+      name: '',
+      city: '',
       type:'',
       breweries: [],
-      reviews: [], 
     };
-    },
-async mounted() {
-  
   },
   methods: {
-    
-    calculateAverageRating(breweryId) {
-      
-        
-      const breweryReviews = this.reviews.filter(review => review.breweryId === breweryId);
-      if (breweryReviews.length > 0) {
-        const total = breweryReviews.reduce((acc, review) => acc + review.rating, 0);
-        return (total / breweryReviews.length).toFixed(1);
-      }
-      return 'No reviews yet'; // If no reviews for the brewery
+    calculateAverageRating(reviews) {
+      let sum = 0;
+      reviews.forEach(review => {
+        sum += review.rating;
+      });
+      return reviews.length > 0 ? sum / reviews.length : 0;
     },
-    
     viewBrewery(brewery) {
       // Set the selected brewery and navigate to the brewery details page
       this.$router.push({ name: 'DetailView', params: { id: brewery.id } });
         },
     
-    async getBreweriesByCity({city}) {
+    async getBreweriesByCity() {
       try {
         const response = await axios.get(`https://api.openbrewerydb.org/v1/breweries?by_city=${this.city}`);
-        this.breweries= response.data;
+        this.breweries = response.data;
+      this.breweries.forEach(async brewery => {
+      const reviews = await loadReviews(brewery.id);
+      const currentRating = this.calculateAverageRating(reviews);
+      brewery.currentRating = currentRating;
+    });
+    
       } catch (error) {
         console.error('Error:', error);
       }
     },
-    async getBreweriesByName(name) {
+    async getBreweriesByName() {
       try {
         const response = await axios.get(`https://api.openbrewerydb.org/breweries?by_name=${this.name}`);
         this.breweries = response.data;
+        this.breweries.forEach(async brewery => {
+        const reviews = await loadReviews(brewery.id);
+        const currentRating = this.calculateAverageRating(reviews);
+        brewery.currentRating = currentRating;
+      });
       } catch (error) {
         console.error('Error:', error);
       }
     },
-    async getBreweriesByType(type) {
+    async getBreweriesByType() {
       try {
         const response = await axios.get(`https://api.openbrewerydb.org/breweries?by_type=${this.type}`);
         this.breweries = response.data;
+        this.breweries.forEach(async brewery => {
+        const reviews = await loadReviews(brewery.id);
+        const currentRating = this.calculateAverageRating(reviews);
+        brewery.currentRating = currentRating;
+      });
       } catch (error) {
         console.error('Error:', error);
       }
